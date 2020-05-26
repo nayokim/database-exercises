a240da2d01d01aa5d1ecb574620833c4da286f1b
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-05-26 10:26:48 -0500

    post walkthrough

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,13 +1,13 @@
 USE codeup_test_db;
 
--- DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(50) DEFAULT 'NONE',
-    name VARCHAR(50) NOT NULL,
-    release_date INT UNSIGNED NOT NULL,
-    sales DOUBLE UNSIGNED NOT NULL,
-    genre TEXT NOT NULL,
+    artist VARCHAR(100) DEFAULT 'NONE',
+    name VARCHAR(100) NOT NULL,
+    release_date INT NOT NULL,
+    sales DECIMAL (6,3) UNSIGNED NOT NULL,
+    genre varchar (100),
     PRIMARY KEY (id)
 );
