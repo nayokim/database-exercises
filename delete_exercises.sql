5c7c8efdde902aacd60dc9b2761b87501fae3707
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-05-26 20:29:41 -0500

    redo delete exercises

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,32 +1,14 @@
 -- Write SELECT statements for:
 -- Albums released after 1991
-select name
-from albums
-where release_date >=1991;
-
-SELECT * FROM albums
-WHERE release_date > '1991';
-
+select * from albums where release_date > 1991;
 -- Albums with the genre 'disco'
-
-select name
-from albums
-where genre = 'disco';
-
-SELECT * FROM albums
-WHERE genre LIKE '%disco%';
+select * from albums where genre = 'disco';
 -- Albums by 'Whitney Houston' (...or maybe an artist of your choice)
+select * from albums where artist = 'Whitney Houston';
+select * from albums where artist like '%whitney houston%';
+-- Make sure to put appropriate captions before each SELECT.
+-- Convert the SELECT statements to DELETE.
 
-select artist
-from albums
-where artist = 'Whitney Houston';
-
-SELECT * FROM albums
-WHERE artist LIKE '%whitney houston%';
-
-delete from albums where release_date = 1991;
-
+delete from albums where release_date > 1991;
 delete from albums where genre = 'disco';
-
 delete from albums where artist like '%whitney houston%';
-
