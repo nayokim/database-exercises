337994d8c4e6cda6ea4783319dfce2e71402202b
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-05-26 20:23:49 -0500

    redo update_exercise

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,21 +1,25 @@
-Select * from albums;
+-- Write SELECT statements to output each of the following with a caption:
 
-Select * from  albums where release_date < 1980;
-
-Select * from albums where artist = 'Michael Jackson';
-
+-- All albums in your table.
+select * from albums;
+-- All albums released before 1980
+select name from albums where release_date < 1980;
+-- All albums by Michael Jackson
+select * from albums where artist = 'Michael Jackson';
 -- After each SELECT add an UPDATE statement to:
+
 -- Make all the albums 10 times more popular (sales * 10)
 update albums
-Set sales = sales * 10;
-select * from albums;
+set sales = sales * 10;
 
+select * from albums;
 -- Move all the albums before 1980 back to the 1800s.
 update albums
-set release_date= release_date - 100
+set release_date = release_date - 100
 where release_date < 1980;
 
 select * from albums;
+
 -- Change 'Michael Jackson' to 'Peter Jackson'
 
 update albums
@@ -23,3 +27,4 @@ set artist = 'Peter Jackson'
 where artist = 'Michael Jackson';
 
 select * from albums;
+-- Add SELECT statements after each UPDATE so you can see the results of your handiwork.
